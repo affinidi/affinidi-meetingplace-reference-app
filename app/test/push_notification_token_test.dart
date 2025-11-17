fbf7bc848076e08af49e870dc828303a7e836f28
Soroush Yousefpour <soroush.y@affinidi.com>
2025-11-17 14:47:02 +0100

    chore: add delay for network change tests to ensure stability

--- app/test/push_notification_token_test.dart
+++ app/test/push_notification_token_test.dart
@@ -55,6 +55,7 @@ void main() {
 
           final l10n = await getL10n();
           fakeConnectivity.emitConnectivityChange([ConnectivityResult.wifi]);
+          await tester.binding.delayed(const Duration(seconds: 2));
           await tester.pumpAndSettle();
 
           expect(find.text(l10n.networkDisconnected), findsNothing);
@@ -221,6 +222,7 @@ void main() {
 
               fakeConnectivity
                   .emitConnectivityChange([ConnectivityResult.wifi]);
+              await tester.binding.delayed(const Duration(seconds: 2));
               await tester.pumpAndSettle();
 
               expect(fakeMeetingPlaceCoreSDK.tokenRegistrationsAttempts, 2);
