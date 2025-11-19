cd64bffda3d3d3c721abe1ac5a213973fb874f71
Earl Grant Reyes <earl.r@affinidi.com>
2025-11-19 10:55:38 +0800

    chore: upgrade MPX SDK to meeting_place_core: ^0.0.1-dev.12

--- app/lib/application/services/connections_service/connections_service.dart
+++ app/lib/application/services/connections_service/connections_service.dart
@@ -206,10 +206,7 @@ class ConnectionsService extends _$ConnectionsService {
         name: _logKey,
       );
 
-      await sdk.approveConnectionRequest(
-        channel: channel,
-        connectionOffer: connectionOffer,
-      );
+      await sdk.approveConnectionRequest(channel: channel);
 
       _logger.info(
         'Connection request approved successfully',
@@ -253,14 +250,12 @@ class ConnectionsService extends _$ConnectionsService {
     required Identity identity,
   }) async {
     final sdk = await ref.read(meetingPlaceSdkProvider.future);
-    final result = await sdk.acceptOffer(
+    await sdk.acceptOffer(
       connectionOffer: connectionOffer,
       vCard: identity.card.toVCard(),
       externalRef: identity.id,
+      senderInfo: identity.card.firstName,
     );
-    await sdk.notifyAcceptance(
-        connectionOffer: result.connectionOffer,
-        senderInfo: identity.card.firstName);
     await fetchConnections();
   }
 
