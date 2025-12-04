bf88cc5b2e529dcbf179e6e758ff769611ecf4ea
Marco Attanasio <marco.a@affinidi.com>
2025-12-04 13:41:09 +0100

    chore: use FakeCacheManager during tests

---
+++ app/test/fakes/fake_cache_manager.dart
@@ -0,0 +1,30 @@
+import 'dart:typed_data';
+
+import 'package:file/file.dart';
+import 'package:file/memory.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class FakeCacheManager implements BaseCacheManager {
+  @override
+  Future<FileInfo?> getFileFromCache(String key,
+      {bool ignoreMemCache = false}) async {
+    return null;
+  }
+
+  @override
+  Future<File> putFile(
+    String url,
+    Uint8List fileBytes, {
+    String? key,
+    String? eTag,
+    Duration maxAge = const Duration(days: 30),
+    String fileExtension = 'file',
+  }) async {
+    return MemoryFileSystem().file('');
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    throw UnimplementedError();
+  }
+}
