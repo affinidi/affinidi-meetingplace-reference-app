c7f880c0f14e5495b50a4fb46049067b5008c867
Vo Thanh Dang Khoa <khoa.v@affinidi.com>
2025-12-04 14:06:31 +0800

    chore: remove unused methods

--- app/test/fakes/fake_meeting_place_sdk.dart
+++ app/test/fakes/fake_meeting_place_sdk.dart
@@ -4,55 +4,6 @@ import 'package:meeting_place_core/meeting_place_core.dart';
 
 import 'fake_publish_offer_result.dart';
 
-class FakeCoreSDKStreamSubscription
-    implements CoreSDKStreamSubscription<MediatorMessage> {
-  final _controller = StreamController<MediatorMessage>.broadcast();
-  bool _isClosed = false;
-
-  // Allow external code to add messages to the stream
-  void addMessage(dynamic message) {
-    if (!_isClosed && message is MediatorMessage) {
-      _controller.add(message);
-    }
-  }
-
-  @override
-  Stream<MediatorMessage> get stream => _controller.stream;
-
-  @override
-  bool get isClosed => _isClosed;
-
-  @override
-  StreamSubscription<MediatorMessage> listen(
-    void Function(MediatorMessage)? onData, {
-    Function? onError,
-    void Function()? onDone,
-    bool? cancelOnError,
-  }) {
-    return _controller.stream.listen(
-      onData,
-      onError: onError,
-      onDone: onDone,
-      cancelOnError: cancelOnError,
-    );
-  }
-
-  @override
-  StreamSubscription<MediatorMessage> timeout(
-    Duration duration, [
-    void Function()? onTimeout,
-  ]) {
-    // Return the stream subscription directly
-    return _controller.stream.listen(null);
-  }
-
-  @override
-  Future<void> dispose() async {
-    _isClosed = true;
-    await _controller.close();
-  }
-}
-
 class FakeMeetingPlaceSDK implements MeetingPlaceCoreSDK {
   FakeMeetingPlaceSDK({
     bool shouldFailToRegisterPushToken = false,
@@ -73,10 +24,8 @@ class FakeMeetingPlaceSDK implements MeetingPlaceCoreSDK {
   final Exception? _publishOfferException;
   final bool _isPhraseAvailable;
   final Map<String, Channel> _channels;
-  final Map<String, FakeCoreSDKStreamSubscription> _subscriptions = {};
 
   // Getter to check if subscriptions have been created (useful for debugging)
-  int get subscriptionCount => _subscriptions.length;
   final ConnectionOffer? offerToFind;
   final bool findOfferHasError;
 
@@ -85,16 +34,6 @@ class FakeMeetingPlaceSDK implements MeetingPlaceCoreSDK {
   @override
   Stream<ControlPlaneStreamEvent> get controlPlaneEventsStream =>
       _controlPlaneEventStreamManager.stream;
-
-  // Allow tests to simulate receiving messages
-  // PlainTextMessage extends MediatorMessage so we can accept it and add it
-  // to the stream
-  void simulateIncomingMessage(String channelDid, PlainTextMessage message) {
-    final subscription = _subscriptions[channelDid];
-    if (subscription != null) {
-      subscription.addMessage(message);
-    }
-  }
 
   String? _lastRegisteredToken;
   String? get lastRegisteredToken => _lastRegisteredToken;
@@ -219,42 +158,6 @@ class FakeMeetingPlaceSDK implements MeetingPlaceCoreSDK {
   Future<Channel?> getChannelByOtherPartyPermanentDid(String channelDid) async {
     final channel = _channels[channelDid];
     return channel;
-  }
-
-  @override
-  Future<CoreSDKStreamSubscription<MediatorMessage>> subscribeToMediator(
-    String channelDid, {
-    String? mediatorDid,
-  }) async {
-    // Create and store a subscription for this channel
-    final subscription = FakeCoreSDKStreamSubscription();
-    _subscriptions[channelDid] = subscription;
-    return subscription;
-  }
-
-  @override
-  Future<List<MediatorMessage>> fetchMessages({
-    required String did,
-    String? mediatorDid,
-    bool deleteOnRetrieve = false,
-    bool deleteFailedMessages = false,
-  }) async {
-    // Return empty list - no messages in fake
-    return [];
-  }
-
-  @override
-  Future<void> sendMessage(
-    PlainTextMessage message, {
-    required String senderDid,
-    required String recipientDid,
-    String? mediatorDid,
-    bool? ephemeral,
-    int? forwardExpiryInSeconds,
-    String? notifyChannelType,
-  }) async {
-    // Fake implementation - just return successfully
-    return;
   }
 
   @override
