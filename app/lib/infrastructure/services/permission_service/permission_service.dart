5e9d41e25b12ffe3eebe2f8a9228deec9f6759cf
Vo Thanh Dang Khoa <khoa.v@affinidi.com>
2025-12-31 18:50:15 +0800

    chore: remove comment

--- app/lib/infrastructure/services/permission_service/permission_service.dart
+++ app/lib/infrastructure/services/permission_service/permission_service.dart
@@ -5,19 +5,16 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'permission_service.g.dart';
 
 /// A service for checking and requesting permissions.
-/// This abstraction allows for easier testing by providing a mockable interface.
 @riverpod
 PermissionService permissionService(Ref ref) {
   return PermissionService();
 }
 
 class PermissionService {
-  /// Gets the current camera permission status.
   Future<PermissionStatus> getCameraPermissionStatus() async {
     return await Permission.camera.status;
   }
 
-  /// Requests camera permission from the user.
   Future<PermissionStatus> requestCameraPermission() async {
     return await Permission.camera.request();
   }
