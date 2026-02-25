f80a04e8d86a1d4e1d722f33702f01a6a839b7ba
Soroush Yousefpour <soroush.y@affinidi.com>
2026-02-25 08:40:26 +0100

    feat: optimize chat initialization performance

--- app/lib/presentation/screens/chat/chat_screen_controller.dart
+++ app/lib/presentation/screens/chat/chat_screen_controller.dart
@@ -315,6 +315,7 @@ class ChatScreenController extends _$ChatScreenController {
       name: _logKey,
     );
     await _startChatSession(contact);
+    await _restoreUnsentMessage();
 
     // TODO(MA): Remove sorting when it's added to sdk
     state = state.copyWith(
@@ -984,16 +985,20 @@ class ChatScreenController extends _$ChatScreenController {
         .updateContactSequenceNumber(channelDid, channel.seqNo);
   }
 
-  Future<void> _startChatSession(Contact contact) async {
+  Future<void> _restoreUnsentMessage() async {
+    final contact = state.contact;
+    if (contact == null) return;
+
     final unsentMessagesService =
         ref.read(unsentMessagesServiceProvider.notifier);
     await unsentMessagesService.ensureInitialized();
-
     final unsentMessage = unsentMessagesService.getUnsentMessage(contact.id);
     if (unsentMessage != null) {
       messageTextController.text = unsentMessage;
     }
+  }
 
+  Future<void> _startChatSession(Contact contact) async {
     state = state.copyWith(isInitialized: true);
 
     final channelDid = contact.channelDid;
@@ -1008,7 +1013,7 @@ class ChatScreenController extends _$ChatScreenController {
         .read(contactsServiceProvider.notifier)
         .resetContactBadgeCount(channelDid);
 
-    await ref.read(appBadgeServiceProvider).clearBadge();
+    unawaited(ref.read(appBadgeServiceProvider).clearBadge());
 
     _logger.info(
       'Chat session started',
