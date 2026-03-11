40b5122ecc07a58e5cfd49c1fd0b55a4b34ea978
Earl Grant Reyes <earl.r@affinidi.com>
2026-03-11 15:32:01 +0800

    fix: initial refactor, introduced ChatService inteface

---
+++ app/lib/application/services/chat_service/app_chat_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_chat_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appChatServiceHash() => r'9633670ce3dbcddc5a741481ad74f3ca4cfe9d91';
+
+/// See also [AppChatService].
+@ProviderFor(AppChatService)
+final appChatServiceProvider =
+    AutoDisposeNotifierProvider<AppChatService, void>.internal(
+      AppChatService.new,
+      name: r'appChatServiceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$appChatServiceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$AppChatService = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
