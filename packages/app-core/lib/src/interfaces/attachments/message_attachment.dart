cb266482640d8441597f30e55b216c455a42185b
Sebastian Rappold <sebastian.m@affinidi.com>
2026-05-07 09:43:39 +0200

    fix: align with type changes on SDK

--- packages/app-core/lib/src/interfaces/attachments/message_attachment.dart
+++ packages/app-core/lib/src/interfaces/attachments/message_attachment.dart
@@ -1,4 +1,5 @@
 import 'package:meeting_place_chat/meeting_place_chat.dart';
+import '../../../mpx_app_core.dart';
 
 abstract interface class MessageAttachment {
   MessageAttachment({required this.pluginName});
