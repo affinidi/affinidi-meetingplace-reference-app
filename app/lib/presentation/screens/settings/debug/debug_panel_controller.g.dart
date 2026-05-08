5428af0dfd759d930f1c77a7ef917ef9842b17a7
Sebastian Rappold <56207234+maindotdev@users.noreply.github.com>
2026-05-08 15:01:40 +0200

    fix: align with transport-agnostic type changes on SDK (#112)

--- app/lib/presentation/screens/settings/debug/debug_panel_controller.g.dart
+++ app/lib/presentation/screens/settings/debug/debug_panel_controller.g.dart
@@ -9,6 +9,42 @@ part of 'debug_panel_controller.dart';
 String _$debugPanelControllerHash() =>
     r'39bc1b2be66c011c8d0d232d87f48de32465edef';
 
+@ProviderFor(DebugPanelController)
+final debugPanelControllerProvider = DebugPanelControllerProvider._();
+
+final class DebugPanelControllerProvider
+    extends $NotifierProvider<DebugPanelController, DebugPanelState> {
+  DebugPanelControllerProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'debugPanelControllerProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$debugPanelControllerHash();
+
+  @$internal
+  @override
+  DebugPanelController create() => DebugPanelController();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(DebugPanelState value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<DebugPanelState>(value),
+    );
+  }
+}
+
+>>>>>>> 0452086 (fix: align with transport-agnostic type changes on SDK (#112))
+String _$debugPanelControllerHash() =>
+    r'39bc1b2be66c011c8d0d232d87f48de32465edef';
+
 /// See also [DebugPanelController].
 @ProviderFor(DebugPanelController)
 final debugPanelControllerProvider =
