d6d1b1d35505a2536dd8129cb0b93fc6d8e41c3d
Soroush Yousefpour <soroush.y@affinidi.com>
2026-05-12 13:02:35 +0200

    chore: update dependencies and add meeting place packages from Git

--- app/lib/infrastructure/extensions/widget_ref_extensions.dart
+++ app/lib/infrastructure/extensions/widget_ref_extensions.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart' show ProviderListenable;
 
 /// Extension methods on [WidgetRef] for managing provider lifecycles.
 extension WidgetRefExtensions on WidgetRef {
   /// Maintains the state of the provider alive for the scope of the widget.
-  void keepAround<T>(ProviderListenable<T> provider) {
-    listen<T>(provider, (_, _) {
+  void keepAround(ProviderListenable<Object?> provider) {
+    listen<Object?>(provider, (_, _) {
       // no-ops
     });
   }
