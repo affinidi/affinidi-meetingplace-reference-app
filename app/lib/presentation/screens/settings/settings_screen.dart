8524c920215595164cc0744b532168c885265e39
abrishs <abrish.s@affinidi.com>
2026-05-12 16:43:24 +0200

    fix: update zkp package
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue May 12 16:43:24 2026 +0200
    #
    # On branch fix-hamburger-menu
    # Your branch is up to date with 'origin/fix-hamburger-menu'.
    #
    # Changes to be committed:
    #	modified:   app/pubspec.yaml
    #
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue May 12 16:43:24 2026 +0200
    #
    # On branch fix-hamburger-menu
    # Your branch is up to date with 'origin/fix-hamburger-menu'.
    #
    # Changes to be committed:
    #	modified:   app/lib/presentation/screens/settings/settings_screen.dart
    #	modified:   app/lib/presentation/widgets/section_banner.dart
    #	modified:   app/pubspec.yaml
    #
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue May 12 16:43:24 2026 +0200
    #
    # On branch fix-hamburger-menu
    # Your branch is up to date with 'origin/fix-hamburger-menu'.
    #
    # Changes to be committed:
    #	modified:   app/lib/presentation/screens/settings/settings_screen.dart
    #	modified:   app/lib/presentation/widgets/section_banner.dart
    #	modified:   app/pubspec.yaml
    #

--- app/lib/presentation/screens/settings/settings_screen.dart
+++ app/lib/presentation/screens/settings/settings_screen.dart
@@ -3,10 +3,12 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../domain/models/mediator/mediator_type.dart';
 import '../../../infrastructure/extensions/build_context_extensions.dart';
+import '../../../navigation/routes/route_paths.dart';
 import '../../dialogs/qr_code_picker/qr_code_picker.dart';
 import '../../dialogs/settings/delete_mediator_dialog.dart';
 import '../../dialogs/settings/rename_mediator_dialog.dart';
@@ -72,6 +74,14 @@ class SettingsScreen extends HookConsumerWidget {
             SectionBanner(
               title: l10n.tabsTitle('settings'),
               subtitle: l10n.settingsScreenSubtitle,
+              onClose: () {
+                final router = GoRouter.of(context);
+                if (router.canPop()) {
+                  router.pop();
+                } else {
+                  router.go(RoutePaths.contacts);
+                }
+              },
             ),
             const SettingsPanelScrollContent(),
           ],
