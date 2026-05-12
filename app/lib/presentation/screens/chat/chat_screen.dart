bde66566e2a486b4176ba65542d8b6c98bc3ae03
abrishs <abrish.s@affinidi.com>
2026-05-12 15:10:53 +0200

    chore: analysis

--- app/lib/presentation/screens/chat/chat_screen.dart
+++ app/lib/presentation/screens/chat/chat_screen.dart
@@ -19,7 +19,6 @@ import 'package:mpx_app_core/mpx_app_core.dart';
 import '../../../application/services/zkp_service/zkp_constants.dart';
 import '../../../domain/models/chat/encryption_notice.dart';
 import '../../../domain/models/contacts/contact_presence_status.dart';
-import '../../../domain/models/contacts/contact_status.dart';
 import '../../../domain/models/contacts/contact_type.dart';
 import '../../../domain/models/identity/identity.dart';
 import '../../../infrastructure/configuration/environment.dart';
