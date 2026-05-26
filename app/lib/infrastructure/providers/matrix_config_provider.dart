98d9665ead0edab53aad4cb46feada4890a87a93
Sebastian Rappold <sebastian.m@affinidi.com>
2026-05-26 12:05:33 +0200

    chore: rebase

--- app/lib/infrastructure/providers/matrix_config_provider.dart
+++ app/lib/infrastructure/providers/matrix_config_provider.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meeting_place_core/meeting_place_core.dart';
 import 'package:path/path.dart' as p;
 import 'package:sqflite_sqlcipher/sqflite.dart' as sqlite;
+
 import '../../application/services/settings_service/settings_service.dart';
 import '../configuration/environment.dart';
 import '../secure_storage/secure_storage.dart';
@@ -62,7 +63,7 @@ MatrixMediaCacheConfig matrixMediaCacheConfig({
 final matrixConfigProvider = FutureProvider<MatrixConfig>((ref) async {
   final environment = ref.read(environmentProvider);
   final settingsState = ref.read(settingsServiceProvider);
-  final homeserver = ref.read(environmentProvider).matrixHomeserver;
+  final homeserver = environment.matrixHomeserver;
   final directory = await ref.read(
     applicationDocumentsDirectoryProvider.future,
   );
