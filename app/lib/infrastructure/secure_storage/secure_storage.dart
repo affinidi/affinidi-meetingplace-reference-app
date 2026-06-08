fe3a6a9ce2d5c5c5f8727c692c359154da9aa382
Marco Attanasio <marco.a@affinidi.com>
2026-06-08 23:25:07 +0200

    chore: upgrade plugins to a version that supports Built-in Kotlin
    Note:
    
    plugins
    app_badge_plus, circom_witnesscalc, flutter_rapidsnark,
    image_picker_android, mobile_scanner, package_info_plus, share_plus
    still don't have a version available that supports built-in Kotlin
    
    clear_all_notifications
    still doesn't support Swift Package Manager for ios

--- app/lib/infrastructure/secure_storage/secure_storage.dart
+++ app/lib/infrastructure/secure_storage/secure_storage.dart
@@ -41,7 +41,7 @@ class SecureStorage implements KeyRepository, KeyStore {
     : _secureStorage =
           secureStorage ??
           const FlutterSecureStorage(
-            aOptions: AndroidOptions(encryptedSharedPreferences: true),
+            aOptions: AndroidOptions(),
             iOptions: IOSOptions(
               accessibility: KeychainAccessibility.unlocked_this_device,
               synchronizable: false,
