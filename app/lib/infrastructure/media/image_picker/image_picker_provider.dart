0c6b307907d8920ea8820543a55875f6afdc556d
Soroush Yousefpour <soroush.y@affinidi.com>
2026-06-23 11:11:44 +0200

    fix: use Android Photo Picker so only images and videos are selectable

--- app/lib/infrastructure/media/image_picker/image_picker_provider.dart
+++ app/lib/infrastructure/media/image_picker/image_picker_provider.dart
@@ -1,9 +1,15 @@
 import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_android/image_picker_android.dart';
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'image_picker_provider.g.dart';
 
 @riverpod
 ImagePicker imagePicker(Ref ref) {
+  final platform = ImagePickerPlatform.instance;
+  if (platform is ImagePickerAndroid) {
+    platform.useAndroidPhotoPicker = true;
+  }
   return ImagePicker();
 }
