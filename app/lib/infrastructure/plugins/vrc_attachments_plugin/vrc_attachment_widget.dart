a4ce2b880a07c7c6c6c65c78d14a6825aedf9f31
Marco Attanasio <marco.a@affinidi.com>
2026-07-03 10:11:23 +0200

    chore: move VrcAttachmentWidget to VrcAttachmentPlugin

--- app/lib/infrastructure/plugins/vrc_attachments_plugin/vrc_attachment_widget.dart
+++ app/lib/infrastructure/plugins/vrc_attachments_plugin/vrc_attachment_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-import '../../../../infrastructure/extensions/build_context_extensions.dart';
+import '../../extensions/build_context_extensions.dart';
 
-// TODO (earl): Rename to `VrcChatItem` for consistency
-class CredentialAttachmentWidget extends StatelessWidget {
-  const CredentialAttachmentWidget({super.key, required this.onTap});
+class VrcAttachmentWidget extends StatelessWidget {
+  const VrcAttachmentWidget({super.key, required this.onTap});
 
   final VoidCallback? onTap;
 
